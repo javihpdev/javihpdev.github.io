122bf3eaca06be9cbb5726688c19f8ef7c78c4c9
Javi Hernandez Perez <javiperezopos@gmail.com>
2025-03-21 20:02:08 +0000

    mas cambios para subir la pagina

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import Nav from './lib/common/Nav.tsx';
-import './index.css';
-import AboutMe from './routes/about-me/AboutMe.tsx';
-import Contacto from './routes/contacto/Contacto.tsx';
-import Inicio from './routes/Inicio/Inicio.tsx';
-import Estudios from './routes/estudios/Estudios.tsx';
-import Experiencia from './routes/experiencia/Experiencia.tsx';
-import Proyectos from './routes/proyectos/Proyectos.tsx';
+import { createRoot } from "react-dom/client";
+import Nav from "./lib/common/Nav";
+import AboutMe from "./routes/about-me/AboutMe";
+import Contacto from "./routes/contacto/Contacto";
+import Estudios from "./routes/estudios/Estudios";
+import Experiencia from "./routes/experiencia/Experiencia";
+import Inicio from "./routes/Inicio/Inicio";
+import Proyectos from "./routes/proyectos/Proyectos";
+
 
 createRoot(document.getElementById('porfolio')!).render(
   <div className="w-full min-h-screen bg-gradient-to-br from-gray-600 via-gray-700 to-gray-800">
