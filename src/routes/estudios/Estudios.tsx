949ade51f34ce951fccf7313dcd6d6ebd0c94938
Javi Hernandez Perez <javiperezopos@gmail.com>
2025-04-02 11:18:32 +0100

    Nuevo diseño para experiencia profesional

--- src/routes/estudios/Estudios.tsx
+++ src/routes/estudios/Estudios.tsx
@@ -43,14 +43,16 @@ function Estudios() {
       className="fade-in flex flex-col w-full md:min-h-screen md:pt-20 justify-center text-center items-center gap-3 opacity-0 transition-opacity duration-4000"
     >
       <article className="flex flex-col w-full max-w-[95%] justify-center items-center gap-10 shadow-[0_0_25px_5px] shadow-[#3D3027] p-20">
-        <h1 className="text-5xl">Estudios</h1>
-        <p className="flex md:w-full w-70 flex-col gap-2 justify-center items-center">
-          Formación Académica: Desarrollo de Aplicaciones Web (Grado Superior):
-          LinkiaFP. Sólidos conocimientos en lenguajes de programación web:
-          JavaScript: Desarrollo de lógica interactiva en el lado del cliente. PHP:
-          Desarrollo de lógica del lado del servidor. HTML: Estructuración de
-          contenido web. CSS: Estilos y diseño visual de páginas web.
-        </p>
+        <h1 className="text-5xl font-bold">Estudios</h1>
+        <ul className="flex md:w-full w-70 flex-col gap-2 justify-center items-center">
+          <li >Desarrollo de Aplicaciones Web (Grado Superior)</li>
+          <li className="text-2xl font-bold">LinkiaFP</li>
+          <ul>Conocimientos en lenguajes de programación web:</ul>
+          <li>JavaScript: Desarrollo de lógica interactiva en el lado del cliente.</li> 
+          <li>HTML: Estructuración de contenido web.</li>
+          <li>CSS: Estilos y diseño visual de páginas web.</li>
+          <li>PHP: Desarrollo de lógica del lado del servidor. </li>
+        </ul>
         <a href="https://linkiafp.es/?utm_source=google&utm_medium=cpc&utm_campaign=Search_bofu_ON_ES_AO_Pros_Brand_Generic&gad_source=1" target="_blank">
           <img src="./images/nlogo-linkia.webp" alt="logo linkiafp" className="w-50" />
         </a>
--- src/routes/experiencia/Experiencia.tsx
+++ src/routes/experiencia/Experiencia.tsx
@@ -1,61 +1,90 @@
 import { useRef, useState, useEffect } from "react";
 
-function Experiencia(){
-    const experienciaRef = useRef(null);
-      const [isScrolling, setIsScrolling] = useState(false);
-    
-      useEffect(() => {
-        const handleScroll = () => {
-          setIsScrolling(true);
-          clearTimeout(window.scrollTimeout);
-          window.scrollTimeout = setTimeout(() => {
-            setIsScrolling(false);
-          }, 4000); // Ajusta el tiempo de espera según sea necesario
-        };
-    
-        window.addEventListener("scroll", handleScroll);
-    
-        const observer = new IntersectionObserver(
-          (entries) => {
-            entries.forEach((entry) => {
-              if (entry.isIntersecting && isScrolling) {
-                entry.target.classList.add("opacity-100");
-              }
-            });
-          });
-    
-        if (experienciaRef.current) {
-          observer.observe(experienciaRef.current);
+function Experiencia() {
+  const experienciaRef = useRef(null);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolling(true);
+      clearTimeout(window.scrollTimeout);
+      window.scrollTimeout = setTimeout(() => {
+        setIsScrolling(false);
+      }, 4000); // Ajusta el tiempo de espera según sea necesario
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && isScrolling) {
+          entry.target.classList.add("opacity-100");
         }
-    
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-            if (experienciaRef.current) {
-              observer.unobserve(experienciaRef.current);
-            }
-          };
-        }, [isScrolling]);
-    return(
-<div id="experiencia" ref={experienciaRef}
+      });
+    });
+
+    if (experienciaRef.current) {
+      observer.observe(experienciaRef.current);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      if (experienciaRef.current) {
+        observer.unobserve(experienciaRef.current);
+      }
+    };
+  }, [isScrolling]);
+  return (
+    <div
+      id="experiencia"
+      ref={experienciaRef}
       className="fade-in flex flex-col w-full md:min-h-screen md:pt-20 justify-center text-center items-center gap-3 opacity-0 transition-opacity duration-4000"
     >
-            <article className="flex flex-col w-full max-w-[95%] justify-center items-center gap-10 shadow-[0_0_25px_5px] shadow-[#3D3027] p-20">
-            <h1 className="flex md:w-full w-70 flex-col gap-2 text-5xl justify-center items-center">Experiencia Profesional</h1>
+      <article className="flex flex-col w-full max-w-[95%] justify-center items-center gap-10 shadow-[0_0_25px_5px] shadow-[#3D3027] p-20">
+        <h1 className="flex md:w-full w-70 flex-col gap-2 text-5xl font-bold justify-center items-center">
+          Experiencia Profesional
+        </h1>
 
-<p className="flex md:w-full w-70 flex-col gap-2 justify-center items-center">
-TipoMedia (Gestión, Diseño y Mantenimiento de Páginas Web):
-Participación activa en el desarrollo y mantenimiento de diversos proyectos web.
-Experiencia en la gestión integral de sitios web, desde el diseño inicial hasta la implementación y el mantenimiento continuo.
-Colaboración en el proyecto "Historiatelo", donde he profundizado en el uso de:
-Svelte 5,
-Tailwind CSS,
-TypeScript.
-</p>
-<a href="https://tipomedia.com/" target="_blank">
-<img src="./images/tipomedia_logo.png" alt="logo tipomedia" className="bg-white p-4 rounded-lg" />
-</a>
-            </article>
-</div>
-    );
-}   
+        <div className="flex md:w-full w-70 flex-col text-lg gap-5 justify-center items-center">
+          TipoMedia (Gestión, Diseño y Mantenimiento de Páginas Web)
+          Participación activa en el desarrollo y mantenimiento de diversos
+          proyectos web. Experiencia en la gestión integral de sitios web, desde
+          el diseño inicial hasta la implementación y el mantenimiento continuo.
+          <span>
+            Colaboración en el proyecto "Historiatelo", donde he profundizado en
+            el uso de:
+          </span>
+          <div className="flex max-w-[95%] md:w-full w-70 md:gap-20 gap-4 justify-center items-center">
+            <img
+              className="w-30 hover:scale-110 transition-all duration-400"
+              src="./images/svelte.png"
+              alt="imagen Svelte"
+            />
+            <img
+              className="w-30 hover:scale-110 transition-all duration-400"
+              src="./images/tailwindcss.svg"
+              alt="imagen Tailwind CSS"
+            />
+            <img
+              className="w-30 hover:scale-110 transition-all duration-400"
+              src="./images/typescript.png"
+              alt="imagen TypeScript"
+            />
+          </div>
+          <div className="flex flex-col justify-center items-center gap- ">
+          <a href="https://tipomedia.com/" target="_blank">
+            <img
+              src="./images/tipomedia_logo.png"
+              alt="logo tipomedia"
+              className="bg-white p-4 rounded-lg"
+            />
+          </a>
+          <span>↑</span>
+          <span>Clica en la imagen para más información</span>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+}
 export default Experiencia;
